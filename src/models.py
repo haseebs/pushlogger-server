32d10b6edbf11ba91cb22e4a9faed2f04ee84a30
Haseeb Shah <haseebs@outlook.com>
2018-05-20 00:44:39 +0500

    Add channel model

--- src/models.py
+++ src/models.py
@@ -1,10 +1,28 @@
 from app import DB
 from datetime import datetime
 
+class Channel(DB.Model):
+    id = DB.Column(DB.Integer, autoincrement=True, unique=True)
+    name = DB.Column(DB.String(16), primary_key=True, index=True)
+    timestamp = DB.Column(DB.DateTime, default=datetime.utcnow)
+    logs = DB.relationship("Logs", cascade="all,delete", backref="channel",
+                           lazy="dynamic")
+
+    def __repr__(self):
+        return '<Channel Name: {}>'.format(self.name)
+
+    def to_json(self):
+        return {
+            "Id": self.id,
+            "Channel Name": self.name,
+            "Timestamp": self.timestamp.strftime("%Y%m%d"),
+        }
+
 class Logs(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     msg = DB.Column(DB.String(128))
-    timestamp = DB.Column(DB.DateTime, index=True, default=datetime.utcnow)
+    timestamp = DB.Column(DB.DateTime, default=datetime.utcnow)
+    channel_id = DB.Column(DB.Integer, db.ForeignKey("Channel.id", ondelete="CASCADE"))
 
     def __repr__(self):
         return '<Log Message: {}>'.format(self.msg)
