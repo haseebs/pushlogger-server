ab7f8a78eac3d4f05fa84cabc4e6b9651c4fc0a0
Haseeb Shah <haseebs@outlook.com>
2018-05-19 21:42:26 +0500

    Added user model

--- src/models.py
+++ src/models.py
@@ -1,5 +1,23 @@
 from app import DB
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin, DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    email = DB.Column(DB.String(120), unique=True, index=True)
+    password_digest = DB.Column(db.String(128))
+    logs = DB.relationship('Logs', cascade="all,delete", backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_digest = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_digest, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)....
+
 
 class Logs(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
