69404864c25040f3cb3b498a226f676b9b86560d
Haseeb Shah <haseebs@outlook.com>
2018-05-20 01:25:26 +0500

    Added channels support

--- src/app.py
+++ src/app.py
@@ -15,24 +15,54 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                           'sqlite:///' + os.path.join(BASEDIR, 'pushlogger.db')
+channels = {}
+
+def get_channel(channel_name):
+    channel_id = None
+    if channel_name in channels:
+        channel_id = channels[channel_name]
+    else:
+        channel = Channel.query.filter_by(name=channel_name)
+        if channel:
+            channels[channel_name] = channel.id
+            channel_id = channels[channel_name]
+    return channel_id
+
+def add_channel(channel_name):
+    channel = Channel(name=channel_name)
+    try:
+        DB.session.add(channel)
+        DB.session.commit()
+        return 200
+    except (exc.SQLAlchemyError, exc.DBAPIError):
+        DB.session.rollback()
+        return 404
+
 
 #--------- Functions for Log ---------#
-def get_logs():
+def get_logs(channel_name):
     '''
     Returns relevant Logs in JSON format
     '''
     msges = []
-    logs = Logs.query.all()
+    channel_id = get_channel(channel_name)
+    if not channel_id:
+        return 404
+    logs = Logs.query.filter_by(channel_id=channel_id)
     for msg in logs:
         msges.append(msg.to_json())
     return jsonify(msges)
 
-def add_logs():
+def add_logs(channel_name):
     '''
     Reads input from message field of POST request
     and adds it to Logs table
     '''
-    log_entry = Logs(msg=request.form['message'])
+    channel_id = get_channel(channel_name)
+    if not channel_id:
+        add_channel(channel_name)
+        channel_id = get_channel(channel_name)
+    log_entry = Logs(msg=request.form['message'], channel_id=channel_id)
     try:
         DB.session.add(log_entry)
         DB.session.commit()
@@ -41,14 +71,17 @@ def add_logs():
         DB.session.rollback()
         return 404
 
+
 #---------Controller for Logs --------#
-@APP.route('/logs/', methods=['GET', 'POST'])
+@APP.route('/logs/<channel_name>', methods=['GET', 'POST'])
 @login_required
-def logs():
-    if request.method == 'GET':
-        return get_logs()
+def logs(channel_name):
+    if not channel_name:
+        return 404
+    elif request.method == 'GET':
+        return get_logs(channel_name)
     elif request.method == 'POST':
-        return add_logs()
+        return add_logs(channel_name)
 
 #-------- Initialize db and APP ---------#
 if __name__ == '__main__':
